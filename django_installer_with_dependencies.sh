929c5ce0ec06afbca27b24d23f06e817b3ca89c5
hugovalcourt <hugo_valcourt@hotmail.com>
2018-12-14 11:07:22 -0500

    Fixed a number typo in the instruction

--- django_installer_with_dependencies.sh
+++ django_installer_with_dependencies.sh
@@ -79,7 +79,7 @@ echo "$(tput setaf 3) 6. $(tput setab 8)sudo -u postgres psql csbcomren -c 'CREA
 echo "$(tput setaf 7) 7. $(tput setab 8)After, go back into the virtual environment with the command: $(tput setaf 3)sudo pipenv shell $(tput setaf 7)you are now ready to run the server: $(tput sgr 0)"
 echo "$(tput setaf 3) $(tput setab 8)   python3 manage.py runserver 0:8080$(tput sgr 0)"
 echo ""
-echo "$(tput setaf 7) 9. $(tput setab 8)You will probably have some issues with using the 'View' button, to fix it,$(tput sgr 0)"
+echo "$(tput setaf 7) 8. $(tput setab 8)You will probably have some issues with using the 'View' button, to fix it,$(tput sgr 0)"
 echo "$(tput setaf 7) $(tput setab 8)   you will need to replace every instance of 127.0.0.1 and localhost present in the project to your machine's IP. And don't fortget to change the port if needed!$(tput sgr 0)"
 echo "$(tput setaf 7) $(tput setab 8)   To find there instances, run this command: $(tput sgr 0)"
 echo "$(tput setaf 0) $(tput setab 8)   $(tput setaf 3)sudo grep -r 127.0.0.1$(tput sgr 0)"
